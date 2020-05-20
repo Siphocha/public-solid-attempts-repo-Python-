020ce0ac26d5105274f1ea2223e599ad990efddf
Empathetic-Otaku <62767262+Empathetic-Otaku@users.noreply.github.com>
2020-05-20 11:42:35 +0200

    Update My recursive linear search Algorithm.py

--- My recursive linear search Algorithm.py
+++ My recursive linear search Algorithm.py
@@ -1,8 +1,12 @@
+#defining what the algorithm function is
 def linear_search(arr,p):
+#used "i" as a way of expressing the potential integer
     for i in range(len(arr)):
         if arr[i] == p:
             return i
     return p-1
+#we returned "p-1" because p is the user integer. So whatever they put minus .
+#this gets the index value. (positioning).
 arr = ["1","2","3","4", "5", "6", "7", "8", "9", "10"]
 p = int(input("What is the number you looking for hime? "))
 print("Element was found at the index " +str(linear_search(arr, p)))
